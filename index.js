da87f4084a3e136abcca355cace23dac799aa368
Aman Kumar <aman.kmr4@yahoo.com>
2020-10-18 22:36:34 +1100

    added error catch and Git hub API response

--- index.js
+++ index.js
@@ -2,161 +2,173 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require('fs');
+const { Console } = require("console");
 
 async function init() {
 
-    const userInput = await
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    message: "What your project Title?",
-                    name: "title"
-                },
-                {
-                    type: "input",
-                    message: "Enter the description of your project",
-                    name: "description"
-                },
-                {
-                    type: "input",
-                    message: "What is steps do you require to install this project? ",
-                    name: "installation"
-                },
+    try {
 
-                {
-                    type: "input",
-                    message: "What is steps for usage of this project? ",
-                    name: "usage"
-                },
+        const userInput = await
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What your project Title?",
+                        name: "title"
+                    },
+                    {
+                        type: "input",
+                        message: "Enter the description of your project",
+                        name: "description"
+                    },
+                    {
+                        type: "input",
+                        message: "What is steps do you require to install this project? ",
+                        name: "installation"
+                    },
 
-                {
-                    type: "input",
-                    message: "Please enter contribution guidelines",
-                    name: "contributing"
-                },
+                    {
+                        type: "input",
+                        message: "What is steps for usage of this project? ",
+                        name: "usage"
+                    },
 
-                {
-                    type: "input",
-                    message: "Please enter the test instruction for this project",
-                    name: "test"
-                },
+                    {
+                        type: "input",
+                        message: "Please enter contribution guidelines",
+                        name: "contributing"
+                    },
+
+                    {
+                        type: "input",
+                        message: "Please enter the test instruction for this project",
+                        name: "test"
+                    },
 
 
-                {
-                    type: "list",
-                    name: "license",
-                    message: "please choise which license you will be using",
-                    choices: ["MIT", "Apache License 2.0", "Mozilla Public License 2.0"]
+                    {
+                        type: "list",
+                        name: "license",
+                        message: "please choise which license you will be using",
+                        choices: ["MIT", "Apache License 2.0", "Mozilla Public License 2.0"]
 
-                },
+                    },
 
-                {
-                    type: "input",
-                    message: "What is your user name?",
-                    name: "username"
-                },
+                    {
+                        type: "input",
+                        message: "What is your user name?",
+                        name: "username"
+                    },
 
-                {
-                    type: "input",
-                    message: "Please enter your email address",
-                    name: "email"
-                },
+                    {
+                        type: "input",
+                        message: "Please enter your email address",
+                        name: "email"
+                    },
 
-            ])
-    //Putting all the inputs from the prompt into an constanent and it it will be called back later
-    console.log(userInput)
-    const projectTitle = userInput.title
-    const projectDescription = userInput.description
-    const projectInstallation = userInput.installation
-    const projectTest = userInput.test
-    const projectUsage = userInput.usage
-    const projectContributingGuideline = userInput.contributing
-    const projectLicense = userInput.license
-    const gitUSername = userInput.username
-    const userEmail = userInput.email
+                ])
+        //Putting all the inputs from the prompt into an constanent and it it will be called back later
+        console.log(userInput)
+        const projectTitle = userInput.title
+        const projectDescription = userInput.description
+        const projectInstallation = userInput.installation
+        const projectTest = userInput.test
+        const projectUsage = userInput.usage
+        const projectContributingGuideline = userInput.contributing
+        const projectLicense = userInput.license
+        const gitUSername = userInput.username
+        const userEmail = userInput.email
 
-    // this function will check what license is chosen
-    licenseOutput = () => {
-        if (projectLicense == "MIT") {
-            const licenseLink = "https://opensource.org/licenses/MIT"
-            return licenseLink
-        } else if (projectLicense == "Apache License 2.0") {
-            const licenseLink = "https://opensource.org/licenses/Apache-2.0"
-            return licenseLink
-        } else if (projectLicense == "Apache License 2.0") {
-            const licenseLink = "https://opensource.org/licenses/MIT"
-            return licenseLink
+        // this function will check what license is chosen
+        licenseOutput = () => {
+            if (projectLicense == "MIT") {
+                const licenseLink = "https://opensource.org/licenses/MIT"
+                return licenseLink
+            } else if (projectLicense == "Apache License 2.0") {
+                const licenseLink = "https://opensource.org/licenses/Apache-2.0"
+                return licenseLink
+            } else if (projectLicense == "Apache License 2.0") {
+                const licenseLink = "https://opensource.org/licenses/MIT"
+                return licenseLink
+            }
         }
-    }
 
-    const licenseLink = await licenseOutput();
+        const licenseLink = await licenseOutput();
 
-    console.log(licenseLink)
-
-
+        //We will now call out github API for the github details
+        const gitOutput = await axios.get(`https://api.github.com/users/${gitUSername}`);
 
 
+        const gitData = gitOutput.data
 
-    //Here we will output the markdown file
-    var output = (`
-# ${projectTitle}
-
-## Table Of Contents
-
-[Description](#description)
-
-[Installation](#installation)
-
-[Usage](#usage)
-
-[Test](#test)
-
-[Contributing Guidelines](#Contributing-Guidelines)
-
-[License](#license)
-
-[Questions](#questions)
-
-## Description
-${projectDescription}
+        console.log(gitData)
 
 
 
-## Installation
-${projectInstallation}
+
+        //Here we will output the markdown file
+        var output = (`
+    # ${projectTitle}
+
+    ## Table Of Contents
+
+    [Description](#description)
+
+    [Installation](#installation)
+
+    [Usage](#usage)
+
+    [Test](#test)
+
+    [Contributing Guidelines](#Contributing-Guidelines)
+
+    [License](#license)
+
+    [Questions](#questions)
+
+    ## Description
+    ${projectDescription}
 
 
 
-## Usage
-${projectUsage}
+    ## Installation
+    ${projectInstallation}
 
 
 
-## License
-${projectLicense}
-${licenseLink}
+    ## Usage
+    ${projectUsage}
 
 
 
-## Contributing Guidelines
-${projectContributingGuideline}
-<br>
+    ## License
+    ${projectLicense}
+    ${licenseLink}
 
 
 
-## Test
-${projectTest}
-<br>
+    ## Contributing Guidelines
+    ${projectContributingGuideline}
+    <br>
 
-## Questions
-* If you have any any more question you can email me at ${userEmail}
-* My github profile link[github link]
-`)
 
-    var writeResult = fs.writeFileSync('readMe.md', output)
-    console.log("file generated....")
+
+    ## Test
+    ${projectTest}
+    <br>
+
+    ## Questions
+    * If you have any any more question you can email me at ${userEmail}
+    * My github profile link[github link]
+    `)
+
+        var writeResult = fs.writeFileSync('readMe.md', output)
+
+
+    } catch (error) {
+        console.log(error)
+        Console.log("There was an error !!")
+    };
 
 }
-
 init();

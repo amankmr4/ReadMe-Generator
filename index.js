924b13a8497ebc032fd84550a350c4149cdb84ad
Aman Kumar <aman.kmr4@yahoo.com>
2020-10-19 00:17:37 +1100

    now it generates everything require besides the badge

--- index.js
+++ index.js
@@ -2,173 +2,177 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require('fs');
-const { Console } = require("console");
 
 async function init() {
 
-    try {
+    const userInput = await
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What your project Title?",
+                    name: "title"
+                },
+                {
+                    type: "input",
+                    message: "Enter the description of your project",
+                    name: "description"
+                },
+                {
+                    type: "input",
+                    message: "What is steps do you require to install this project? ",
+                    name: "installation"
+                },
 
-        const userInput = await
-            inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: "What your project Title?",
-                        name: "title"
-                    },
-                    {
-                        type: "input",
-                        message: "Enter the description of your project",
-                        name: "description"
-                    },
-                    {
-                        type: "input",
-                        message: "What is steps do you require to install this project? ",
-                        name: "installation"
-                    },
+                {
+                    type: "input",
+                    message: "What is steps for usage of this project? ",
+                    name: "usage"
+                },
 
-                    {
-                        type: "input",
-                        message: "What is steps for usage of this project? ",
-                        name: "usage"
-                    },
+                {
+                    type: "input",
+                    message: "Please enter contribution guidelines",
+                    name: "contributing"
+                },
 
-                    {
-                        type: "input",
-                        message: "Please enter contribution guidelines",
-                        name: "contributing"
-                    },
-
-                    {
-                        type: "input",
-                        message: "Please enter the test instruction for this project",
-                        name: "test"
-                    },
+                {
+                    type: "input",
+                    message: "Please enter the test instruction for this project",
+                    name: "test"
+                },
 
 
-                    {
-                        type: "list",
-                        name: "license",
-                        message: "please choise which license you will be using",
-                        choices: ["MIT", "Apache License 2.0", "Mozilla Public License 2.0"]
+                {
+                    type: "list",
+                    name: "license",
+                    message: "please choise which license you will be using",
+                    choices: ["MIT", "Apache License 2.0", "Mozilla Public License 2.0"]
 
-                    },
+                },
 
-                    {
-                        type: "input",
-                        message: "What is your user name?",
-                        name: "username"
-                    },
+                {
+                    type: "input",
+                    message: "What is your user name?",
+                    name: "username"
+                },
 
-                    {
-                        type: "input",
-                        message: "Please enter your email address",
-                        name: "email"
-                    },
+                {
+                    type: "input",
+                    message: "Please enter your email address",
+                    name: "email"
+                },
 
-                ])
-        //Putting all the inputs from the prompt into an constanent and it it will be called back later
-        console.log(userInput)
-        const projectTitle = userInput.title
-        const projectDescription = userInput.description
-        const projectInstallation = userInput.installation
-        const projectTest = userInput.test
-        const projectUsage = userInput.usage
-        const projectContributingGuideline = userInput.contributing
-        const projectLicense = userInput.license
-        const gitUSername = userInput.username
-        const userEmail = userInput.email
+            ])
+    //Putting all the inputs from the prompt into an constanent and it it will be called back later
+    console.log(userInput)
+    const projectTitle = userInput.title
+    const projectDescription = userInput.description
+    const projectInstallation = userInput.installation
+    const projectTest = userInput.test
+    const projectUsage = userInput.usage
+    const projectContributingGuideline = userInput.contributing
+    const projectLicense = userInput.license
+    const gitUsername = userInput.username
+    const userEmail = userInput.email
 
-        // this function will check what license is chosen
-        licenseOutput = () => {
-            if (projectLicense == "MIT") {
-                const licenseLink = "https://opensource.org/licenses/MIT"
-                return licenseLink
-            } else if (projectLicense == "Apache License 2.0") {
-                const licenseLink = "https://opensource.org/licenses/Apache-2.0"
-                return licenseLink
-            } else if (projectLicense == "Apache License 2.0") {
-                const licenseLink = "https://opensource.org/licenses/MIT"
-                return licenseLink
-            }
+    licenseOutput = () => {
+        if (projectLicense == "MIT") {
+            const licenseLink = "https://opensource.org/licenses/MIT"
+            return licenseLink
+        } else if (projectLicense == "Apache License 2.0") {
+            const licenseLink = "https://opensource.org/licenses/Apache-2.0"
+            return licenseLink
+        } else if (projectLicense == "Mozilla Public License 2.0") {
+            const licenseLink = "https://opensource.org/licenses/MPL-2.0"
+            return licenseLink
         }
+    }
 
-        const licenseLink = await licenseOutput();
+    const licenseLink = await licenseOutput();
 
+    console.log(licenseLink)
+    try {
         //We will now call out github API for the github details
-        const gitOutput = await axios.get(`https://api.github.com/users/${gitUSername}`);
+        const gitOutput = await axios.get(`https://api.github.com/users/${gitUsername}`);
+        const githubdata = gitOutput.data
+        const githubProfile = githubdata.html_url
+
+        console.log(typeof (githubProfile))
 
 
-        const gitData = gitOutput.data
-
-        console.log(gitData)
 
 
-
-
-        //Here we will output the markdown file
         var output = (`
-    # ${projectTitle}
+# ${projectTitle}
 
-    ## Table Of Contents
+## Table Of Contents
 
-    [Description](#description)
+[Description](#description)
 
-    [Installation](#installation)
+[Installation](#installation)
 
-    [Usage](#usage)
+[Usage](#usage)
 
-    [Test](#test)
+[Test](#test)
 
-    [Contributing Guidelines](#Contributing-Guidelines)
+[Contributing Guidelines](#Contributing-Guidelines)
 
-    [License](#license)
+[License](#license)
 
-    [Questions](#questions)
+[Questions](#questions)
 
-    ## Description
-    ${projectDescription}
-
-
-
-    ## Installation
-    ${projectInstallation}
+## Description
+${projectDescription}
 
 
 
-    ## Usage
-    ${projectUsage}
+## Installation
+${projectInstallation}
 
 
 
-    ## License
-    ${projectLicense}
-    ${licenseLink}
+## Usage
+${projectUsage}
 
 
 
-    ## Contributing Guidelines
-    ${projectContributingGuideline}
-    <br>
+## License
+${projectLicense}
 
 
 
-    ## Test
-    ${projectTest}
-    <br>
+## Contributing Guidelines
+${projectContributingGuideline}
 
-    ## Questions
-    * If you have any any more question you can email me at ${userEmail}
-    * My github profile link[github link]
-    `)
+
+
+
+## Test
+${projectTest}
+
+
+## Questions
+* If you have any any more question you can email me at ${userEmail}
+* My github profile link ${githubProfile}
+
+
+
+
+`)
 
         var writeResult = fs.writeFileSync('readMe.md', output)
-
-
+        console.log("file generated....")
     } catch (error) {
         console.log(error)
-        Console.log("There was an error !!")
-    };
-
+        console.log("there was an error please read above!!")
+    }
 }
+
 init();
+
+
+
+
+
+
